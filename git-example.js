5d16e2175939d0b434e1bc7d646e854317a77171
e-e-e <benjamin.mforster@gmail.com>
2017-10-22 17:06:18 +1100

    update example to try replicating.

--- git-example.js
+++ git-example.js
@@ -1,6 +1,7 @@
 var storage = require('./')
 var hyperdrive = require('hyperdrive')
 var http = require('http-random-access')
+var swarm = require('hyperdiscovery')
 
 var gitInterface = (file) => http(file, { url: 'https://raw.githubusercontent.com/e-e-e/base-dat-storage/master/example-dat/' })
 
@@ -15,7 +16,7 @@ var archive = hyperdrive(gitStorage, {
 })
 
 archive.on('ready', () => {
-  console.log('Git Dat is READY')
+  console.log('Git Dat is READY', archive.key.toString('HEX'))
   archive.readdir('/', (err, list) => {
     if (err) throw err
     console.log('Git Dat base directory:', list)
@@ -23,6 +24,14 @@ archive.on('ready', () => {
       if (err) throw err
       console.log(list[0], 'from Git Dat:')
       console.log(data)
+    })
+  })
+  var sw = swarm(archive)
+  sw.on('connection', function (peer, type) {
+    // console.log('got', peer, type)
+    console.log('connected to', sw.connections.length, 'peers')
+    peer.on('close', function () {
+      console.log('peer disconnected')
     })
   })
 })
